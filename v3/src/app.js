738e9ba8ce4be15a0d689d63a6d652887549f256
heudev <hulkienesuysal@gmail.com>
2024-09-02 08:54:39 +0300

    endpoints updated

--- v3/src/app.js
+++ v3/src/app.js
@@ -14,14 +14,14 @@ app.get("/", (req, res) => {
     <h1>IEU Announcements</h1>
     ENDPOINTS:
     <ul>
-        <li>GET <a href="/api/announcement/en">/api/announcement/en</a></li>
+        <li>GET <a href="/api/announcements/en">/api/announcements/en</a></li>
         <li>GET <a href="/api/news/en">/api/news/en</a></li>
-        <li>GET <a href="/api/sflannouncement/en">/api/sflannouncement/en</a></li>
+        <li>GET <a href="/api/sflannouncements/en">/api/sflannouncements/en</a></li>
     </ul>
     <ul>
-        <li>GET <a href="/api/announcement/tr">/api/announcement/tr</a></li>
+        <li>GET <a href="/api/announcements/tr">/api/announcements/tr</a></li>
         <li>GET <a href="/api/news/tr">/api/news/tr</a></li>
-        <li>GET <a href="/api/sflannouncement/tr">/api/sflannouncement/tr</a></li>
+        <li>GET <a href="/api/sflannouncements/tr">/api/sflannouncements/tr</a></li>
     </ul>
     <p>This application periodically checks the announcements and news of Izmir University of Economics and provides API service. It also shares announcements and news via Telegram channel.</p>
     <p>You can join the Telegram channel <a href="https://telegram.ieu.app">telegram.ieu.app</a> and get IEU notifications instantly.</p>
@@ -31,7 +31,7 @@ app.get("/", (req, res) => {
 app.listen(process.env.APP_PORT, () => {
     console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${process.env.APP_PORT}`);
 
-    app.use("/api/announcement", Announcement);
+    app.use("/api/announcements", Announcement);
     app.use("/api/news", News);
-    app.use("/api/sflannouncement", SflAnnouncement);
+    app.use("/api/sflannouncements", SflAnnouncement);
 });
